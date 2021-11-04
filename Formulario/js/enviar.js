f791d71d08c368c179a4bc026af8d06d5f26e8ee
CesarAugusto <cesinhalius@hotmail.com>
2021-11-04 14:12:55 -0300

    Update files

--- Formulario/js/enviar.js
+++ Formulario/js/enviar.js
@@ -1,11 +1,8 @@
 
 
 function Enviar(){
-    let email = document.getElementById('email').value;
    let nome = document.getElementById('name').value;
     if(nome !== "" ){
-        alert('obrigado por cadeastrar SR.' + nome + email);
-    }else{
-        console.log('oi')
+        alert('obrigado por cadeastrar SR.' + nome);
     }
 }
