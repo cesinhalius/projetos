85c01b8cb2a85eb6563b23701abd42c37b47bd49
CesarAugusto <cesinhalius@hotmail.com>
2021-11-26 16:11:36 -0300

    Update Server

--- dashboard/Back-end/index.js
+++ dashboard/Back-end/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get('/', (res, resp) =>{
+    console.log('servidorchamado');
 
+    return resp.send('Hello');
+});
 
 
 app.listen(2525);
