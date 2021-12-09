95e372cc6a513fbcf93df7b68de57885ff683c90
CesarAugusto <cesinhalius@hotmail.com>
2021-12-09 17:32:54 -0300

    add animation in project

---
+++ Landing_Page/js/app.js
@@ -0,0 +1,41 @@
+var toggle_btn;
+var big_wrapper;
+
+function declare() {
+  big_wrapper = document.querySelector(".big-wrapper");
+  toggle_btn = document.querySelector(".toggle-btn");
+}
+
+const main = document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation() {
+  dark = !dark;
+  let clone = big_wrapper.cloneNode(true);
+  if (dark) {
+    clone.classList.remove("light");
+    clone.classList.add("dark");
+  } else {
+    clone.classList.remove("dark");
+    clone.classList.add("light");
+  }
+  clone.classList.add("copy");
+  main.appendChild(clone);
+
+  clone.addEventListener("animationend", () => {
+    big_wrapper.remove();
+    clone.classList.remove("copy");
+
+    declare();
+    events();
+  });
+}
+
+function events() {
+  toggle_btn.addEventListener("click", toggleAnimation);
+}
+
+events();
